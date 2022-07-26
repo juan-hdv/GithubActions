41f1e9bdeb68618f99ee31ac287eceb623317215
Juan G Mejia <juan.mejia@nomadhealth.com>
2022-07-26 14:52:17 -0500

    Test

--- format.sh
+++ format.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 python3 save_formated_pr_body.py > result.txt
-cat result.txt
-
+FORMATTED_BODY=$(cat result.txt)
+ECHO ---------------------------
+ECHO FORMATTED_BODY
+ECHO ---------------------------
