dad6a8e43e4837c7beda8704727587b2bef4823c
Juan G Mejia <juan.mejia@nomadhealth.com>
2022-07-28 07:44:21 -0500

    Improve

--- .github/workflows/changelog/save_formated_pr_body.py
+++ .github/workflows/changelog/save_formated_pr_body.py
@@ -1,36 +1,57 @@
-import sys
+from enum import IntEnum
 from datetime import datetime, timezone
 import re
+import sys
+
+
+NOMAD_JIRA_URL = "https://nomadhealth.atlassian.net/browse/"
+
+
+class FormatterError(IntEnum):
+
+    def __new__(cls, value, description=''):
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+        obj.description = description
+
+        return obj
+
+    # Informative
+    SUCCESS = 0, "Formating successful"
+
+    # Errors
+    EMPTY_BODY = 300, "Empty body: PR has an empty body"
+    ROBOT_SIGN_MISSING = 320, "Missing sign: robot: auto generated pull request"
+    TITLE_MISSING = 340, "Missing title: Missing # Changes"
+    PATTERN_NOT_FOUND = 360, "Pattern not found: - @user ... [GithubCode](GithubUrl)"
 
 
 class Formatter:
 
+    # https://confluence.atlassian.com/adminjiraserver/changing-the-project-key-format-938847081.html
+    PATTERN_JIRA_CODE = "[a-zA-Z][a-zA-Z0-9_]+-[0-9]+"  # noqa #605
+
     URL_UNKNOWN = "http://unknown"
     PATTERN_USER = "@[a-zA-Z0-9_-]+"  # noqa #605
-    PATTERN_JIRA_CODE = "[a-zA-Z]+-[0-9]+"  # noqa #605
-    PATTERN_GITHUB_CODE = "#\d+?"  # noqa #605
+    PATTERN_GITHUB_CODE = "#\d+"  # noqa #605
     PATTERN_GITHUB_URL = "\(https\:\/\/github\.com\/NomadHealth.+?\)" # noqa #605
 
     def __init__(self, body) -> None:
         self.body = body
 
-    def error(self, comment=None):
-        msg = "Formatting Error:: PR body format not recognized!"
-        msg = f"{msg} {comment if comment else ''}"
-        msg = f"[{msg}]\n\n{self.body}"
+    def clean_characters(self, text, characters):
+        for c in characters:
+            text = text.replace(c, "")
+        return text
+
+    def error(self, error: FormatterError):
+        if error.value == FormatterError.SUCCESS:
+            return "\n"
+
+        err = f"Formatting Error [{error.value}] :: {error.description}"
+        msg = f"{self.body}\n\n{'⎯'*(len(err))}\n{err}\n"
 
         return msg
-
-    def get_pr_jira_url(self, pr_jira_code, jira_urls_list):
-        """ Get the Jira link from the bottom of the body (jira_urls) """
-        result = self.URL_UNKNOWN
-        for url in jira_urls_list:
-            match = re.match(rf"^\[{pr_jira_code}\]:\s*(.+)$", url)
-            if match:
-                result = match.group(1)
-                break
-
-        return result
 
     def format_body(self) -> str:
         """
@@ -53,58 +74,66 @@ class Formatter:
         [Link 1] Description 1 - PR owner
         [Link 2] Description 2 - PR owner
         """
-        if not self.body:
-            self.error("Starting formatting... ")
+        text_to_parse = self.body.strip()
+        if not len(text_to_parse):
+            return self.error(FormatterError.EMPTY_BODY)
 
-        self.body.replace("\r\n", "\n")
-        self.body.replace("\r", "\n")
+        text_to_parse = self.clean_characters(text_to_parse, "`'\"][")
+        text_to_parse = text_to_parse.replace("\r\n", "\n").replace("\r", "\n")
 
         # Get the text with the list of PRs
-        match = re.match(r"^\s*# Changes(.+):robot: auto generated pull request(.+)$", self.body, re.DOTALL) # noqa
-        if not match:
-            return self.error("NOT found: robot: auto generated pull request...")
+        sections = text_to_parse.split(":robot: auto generated pull request")
+        if len(sections) <= 1:
+            return self.error(FormatterError.ROBOT_SIGN_MISSING)
 
-        result = match.group(1)
-        jira_urls_list = (
-            match.group(2).replace(" ", "").replace("`", "").split("\n")
-        )
-        jira_urls_list = [x.rstrip() for x in jira_urls_list if len(x.strip("\n ")) > 0]  # noqa
+        text_to_parse = sections[0]
+        sections = text_to_parse.split("# Changes")
+        if len(sections) <= 1:
+            return self.error(FormatterError.TITLE_MISSING)
+
+        text_to_parse = text_to_parse.lstrip().rstrip()
 
         # Get each of the PR's info
-        pr_matches = re.findall(rf"\s*(-\s{self.PATTERN_USER}.*?\[{self.PATTERN_GITHUB_CODE}\]{self.PATTERN_GITHUB_URL})\n?", result, re.DOTALL)  # noqa
+        pr_matches = re.findall(rf"\s*(-\s{self.PATTERN_USER}.*?{self.PATTERN_GITHUB_CODE}{self.PATTERN_GITHUB_URL})\n?", text_to_parse, re.DOTALL)  # noqa
         if not pr_matches:
-            return self.error(f"PR Partterns not found... ")
+            return self.error(FormatterError.PATTERN_NOT_FOUND)
 
         date_obj = datetime.now(timezone.utc)
         date_str = date_obj.strftime("%m/%d/%Y at %H:%M")
         result_string = (
-            f"There was a Production deployment on {date_str} (UTC), containing the following tickets:\n\n"  # noqa
-            # f"Jira Code | Description | @ Owner | Github Url\n"
-            # f"---------   -----------   -------   ----------\n"
+            f"There was a Production deployment on {date_str} (UTC), "
+            f"containing the following tickets:\n\n"
         )
         for pr in pr_matches:
-            match = re.match(rf"\s*(-\s+{self.PATTERN_USER}.*?\[{self.PATTERN_GITHUB_CODE}\]{self.PATTERN_GITHUB_URL})", pr)  # noqa
+            match = re.match(rf"\s*-\s+({self.PATTERN_USER})\s+(.*)({self.PATTERN_GITHUB_CODE})({self.PATTERN_GITHUB_URL})", pr) # noqa
             if not match:
-                continue
+                return self.error(FormatterError.PATTERN_NOT_FOUND)
 
-            match = re.match(rf"-\s*({self.PATTERN_USER})\s+(\[{self.PATTERN_JIRA_CODE}\])?(.*?)\[({self.PATTERN_GITHUB_CODE})\]({self.PATTERN_GITHUB_URL})", pr) # noqa
             owner = match.group(1)
-            owner = owner.replace("@", "@ ") if owner else "@ unknown"
+            owner = owner.replace("@", "@ ") if owner else "@ Unknown"
 
-            pr_jira_code = match.group(2) or "Unknown"
-            pr_jira_code = pr_jira_code.strip("][")
-
-            description = match.group(3) or ""
+            description = match.group(2) or ""
             description = description.strip()
 
-            pr_github_code = match.group(4) or "Unknown"
+            matches = re.findall(rf"({self.PATTERN_JIRA_CODE})", description)
+            jira_codes_string = ""
+            for jira_code in matches:
+                jira_code_upper = jira_code.upper()
+                jira_url = f"{NOMAD_JIRA_URL}{jira_code_upper}"
+                jira_codes_string += f"<{jira_url}|[{jira_code_upper}]> "
 
-            pr_github_url = match.group(5) or self.URL_UNKNOWN
+            jira_codes_string = jira_codes_string.rstrip()
+            if not jira_codes_string:
+                jira_codes_string += f"<{self.URL_UNKNOWN}|[Unknown]>"
+
+            pr_github_code = match.group(3) or "Unknown"
+            pr_github_url = match.group(4) or self.URL_UNKNOWN
             pr_github_url = pr_github_url.strip(")(\n ")
 
-            pr_jira_url = self.get_pr_jira_url(pr_jira_code, jira_urls_list)
 
-            result_string += f"• <{pr_jira_url}|[{pr_jira_code}]> {description} {owner} <{pr_github_url}|[PR: {pr_github_code}]>\n"  # noqa
+            result_string += f"• {jira_codes_string}: {description} {owner} <{pr_github_url}|[PR: {pr_github_code}]>\n"  # noqa
+
+        result_string += self.error(FormatterError.SUCCESS)
 
         return result_string
 
